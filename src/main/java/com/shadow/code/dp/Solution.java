58ee9c34fea302cc4e4f91001a655fef9b5f4064
flytzuhan <1602535656@qq.com>
2023-04-12 23:42:12 +0800

    记录周一刷的题目

--- src/main/java/com/shadow/code/dp/Solution.java
+++ src/main/java/com/shadow/code/dp/Solution.java
@@ -1,5 +1,7 @@
 package com.shadow.code.dp;
 
+import org.junit.Assert;
+import org.junit.Test;
 import java.util.*;
 
 public class Solution {
@@ -133,5 +135,66 @@ public class Solution {
         }
 
         return map.get(n);
+    }
+
+    /**
+     * 简单题：给一个由整数组成的非空数组，给这个数+1，返回结果，根据题意：其他的情况都比较好处理，只有一个是如果全部是9的情况
+     * 这种就会多出来一位，即最高位为1，其他位全部是0
+     *
+     * @param digits
+     * @return
+     */
+    public int[] plusOne(int[] digits) {
+        // 先循环遍历所有的数字，判断是否为9，如果为9的话，就直接给加上，如果不是的话，
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                // 这样就会在其他的位数不是9的情况下，直接返回了
+                digits[i]++;
+                return digits;
+            } else {
+                // 如果所有的位数都是9的情况下，+1的话，就会使得这些位数都为0
+                digits[i] = 0;
+            }
+        }
+        // 这样就剩下最后一种情况了，所有的位数都是9，+1之后，最高位是1，其余都是0，所以特殊处理这种情况
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        return result;
+    }
+
+    @Test
+    public void testPartitionLabels() {
+        Assert.assertEquals("[9, 7, 8]", partitionLabels("ababcbacadefegdehijhklij").toString());
+    }
+
+    /**
+     * 中等题：划分字母区间，给你一个字符串s，需要将这个字符串划分成尽可能多的片段，同一个字母最多出现在一个片段中，
+     * 根据题意：需要知道每个字符最后出现的位置，可以使用字典进行记录，接着循环这个字符串，根据每个字符的最后出现位置，判断当前循环的字符，
+     * 是否在最大的索引位置内部，如果在的话，以最大的索引位置为准，如果不在，那就是要将最大的索引位置替换成当前的这个字符最后出现的位置上
+     *
+     * @param s
+     * @return
+     */
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> list = new ArrayList<>();
+        // 首先要知道每个字符出现的最后位置
+        Map<Character, Integer> map = new HashMap<>();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            map.put(s.charAt(i), i);
+        }
+
+        // 设置开始和结束的索引位置
+        int start = 0, end = 0;
+        for (int i = 0; i < n; i++) {
+            end = Math.max(end, map.get(s.charAt(i)));
+            if (i == end) {
+                list.add(end - start + 1);
+                start = end + 1;
+            }
+        }
+
+        return list;
     }
 }
