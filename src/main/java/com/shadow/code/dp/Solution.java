661058280c54422e5e1cfbbfd403632e6e54bb8d
flytzuhan <1602535656@qq.com>
2023-05-07 21:49:51 +0800

    记录反转链表的题目解法

--- src/main/java/com/shadow/code/dp/Solution.java
+++ src/main/java/com/shadow/code/dp/Solution.java
@@ -301,4 +301,63 @@ public class Solution {
         }
         return result.next;
     }
+
+    /**
+     * leetcode 206 翻转链表，使用递归的方式翻转
+     *
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        // 使用递归的方式翻转链表
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode last = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+
+        return last;
+    }
+
+    /**
+     * 反转链表中的前n个节点
+     *
+     * @param head
+     * @param n
+     * @return
+     */
+    public ListNode successorNode;
+    public ListNode reverseN(ListNode head, int n) {
+        if (n == 1) {
+            successorNode = head.next;
+            return head;
+        }
+
+        ListNode last = reverseN(head.next, n - 1);
+        head.next.next = head;
+        head.next = successorNode;
+
+        return last;
+    }
+
+    /**
+     * leetcode 92 反转链表中的指定部分
+     *
+     * @param head
+     * @param left
+     * @param right
+     * @return
+     */
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        // 加入left如果是1的话，那就相当于反转前right个节点
+        if (left == 1) {
+            return reverseN(head, right);
+        }
+
+        head.next = reverseBetween(head.next, left - 1, right - 1);
+
+        return head;
+    }
 }
